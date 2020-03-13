df3e4f57f77d3199348918181021ef094b5892dc
Student_GA124 <student_ga124@Student-GA124.local>
2020-03-13 11:50:52 -0400

    Updated CSS to make game fixed position and added more Pseudocode to js

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 //Use this to change the position of the box of data
+// move it by 50px on every button push
 var redBox = document.getElementById("tapOfWarData");
-var vars = 45;
+var vars = 300;
 
-function moveRedBox(){
-    console.log(redBox.style.left = 'calc(50% + ' + vars + '%)');
+// redBox.onkeydown = function moveRedBox () {
+//     redBox.style.left = 'calc(600px + ' + vars + 'px';
+// }
+
+function moveRedBox(e){
+    if(e.keyCode = 65){
+        redBox.style.left = 'calc(600px + ' + vars + 'px';
+    }
+    if(e.keyCode = 75){
+        redBox.style.left = 'calc(600px - ' + vars + 'px';
+    }
 }
+
+
+
+document.onkeydown = moveRedBox;
+
+const log = document.getElementById('redBox');
+
+document.addEventListener('keypress', logKey);
+
 
 //redBox.style.left = 5 + "%";
 
 //Math.random digits form 0 to 9
-console.log(Math.floor(Math.random() * (10 - 0) + 0));
+// console.log(Math.floor(Math.random() * (10 - 0) + 0));
 
 // document.onkeydown = logKey;
 // console.log(document.onkeydown);
 
-moveRedBox();
+
 
