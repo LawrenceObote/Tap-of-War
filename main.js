0c48b59e31cd6b3efc199fb237802def9cb9a1c4
Student_GA124 <student_ga124@Student-GA124.local>
2020-03-13 16:23:17 -0400

    Created Event Listener to move bar

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 //Use this to change the position of the box of data
 // move it by 50px on every button push
-var redBox = document.getElementById("tapOfWarData");
-var vars = 300;
+let redBox = document.getElementById("tapOfWarData");
+let position = 0;
+let arrayLeft = [83, 65, 87, 68];
+let arrayRight = [74, 73, 75, 76];
+
 
 // redBox.onkeydown = function moveRedBox () {
 //     redBox.style.left = 'calc(600px + ' + vars + 'px';
 // }
-
-function moveRedBox(e){
-    if(e.keyCode = 65){
-        redBox.style.left = 'calc(600px + ' + vars + 'px';
+window.addEventListener('keydown', function moveRedBox(e) {
+    if(e.keyCode === 65){
+        position -= 50;
+        redBox.style.left = 'calc(600px + ' + position + 'px';
+        
     }
-    if(e.keyCode = 75){
-        redBox.style.left = 'calc(600px - ' + vars + 'px';
+    if(e.keyCode === 75){
+        position += 50;
+        redBox.style.left = 'calc(600px + ' + position + 'px';
     }
-}
+    else return;
+});
+// function moveRedBox(e){
+//     if(e.keyCode === 65){
+//         redBox.style.left = 'calc(600px - ' + vars + 'px';
+//     }
+//     if(e.keyCode === 75){
+//         redBox.style.left = 'calc(600px + ' + vars + 'px';
+//     }
+//     else return;
+// }
 
 
 
-document.onkeydown = moveRedBox;
+// document.onkeydown = moveRedBox;
 
-const log = document.getElementById('redBox');
+// const log = document.getElementById('redBox');
 
-document.addEventListener('keypress', logKey);
+// document.addEventListener('keypress', logKey);
 
 
 //redBox.style.left = 5 + "%";
 
 //Math.random digits form 0 to 9
-// console.log(Math.floor(Math.random() * (10 - 0) + 0));
+ function random() {
+    Math.floor(Math.random() * (10 - 0) + 0);
+ };
 
 // document.onkeydown = logKey;
 // console.log(document.onkeydown);
