a97bc98ef829e657d7e551a8f98089d5e4d7eac4
Student_GA124 <student_ga124@Student-GA124.local>
2020-03-13 17:07:44 -0400

    Positioned the scoreboard

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Use this to change the position of the box of data
 // move it by 50px on every button push
 let redBox = document.getElementById("tapOfWarData");
+let p1Score = document.getElementById("p1Score");
+let p2Score = document.getElementById("p2Score");
+let countDown = document.getElementById("countDown");
 let position = 0;
 let arrayLeft = [83, 65, 87, 68];
 let arrayRight = [74, 73, 75, 76];
@@ -12,18 +15,39 @@ console.log(randomLeft);
 // redBox.onkeydown = function moveRedBox () {
 //     redBox.style.left = 'calc(600px + ' + vars + 'px';
 // }
-window.addEventListener('keydown', function moveRedBox(e) {
+window.addEventListener('keydown', function moveRedBoxLeft(e) {
 let randomLeft = Math.floor(Math.random() * (3 - 0) + 0);
-console.log(randomLeft);
+
+if (randomLeft === 0){
+    if(e.keyCode === 83){
+        position -= 50;
+        redBox.style.left = 'calc(600px + ' + position + 'px';
+    }
+}
+if (randomLeft === 1){
     if(e.keyCode === 65){
         position -= 50;
         redBox.style.left = 'calc(600px + ' + position + 'px';
-        
     }
-    if(e.keyCode === 75){
-        position += 50;
+} 
+if (randomLeft === 2){
+    if(e.keyCode === 87){
+        position -= 50;
         redBox.style.left = 'calc(600px + ' + position + 'px';
     }
+} 
+if (randomLeft === 3){
+    if(e.keyCode === 68){
+        position -= 50;
+        redBox.style.left = 'calc(600px + ' + position + 'px';
+    }
+}  
+
+
+    // if(e.keyCode === 75){
+    //     position += 50;
+    //     redBox.style.left = 'calc(600px + ' + position + 'px';
+    // }
     else return;
 });
 // function moveRedBox(e){
@@ -54,6 +78,8 @@ console.log(randomLeft);
 
 // document.onkeydown = logKey;
 // console.log(document.onkeydown);
+
+
 
 
 
